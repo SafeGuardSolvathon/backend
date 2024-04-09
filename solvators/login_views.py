8fc9a2816e9b72b05c930e27b4b03fea9e01c4fc
Marushka Fernandes <marusfern@gmail.com>
2024-04-09 14:15:42 +0530

    second

--- solvators/login_views.py
+++ solvators/login_views.py
@@ -9,12 +9,12 @@ from rest_framework import status
 def login(request, format=None):
     if request.method == 'POST':
         # Retrieve username and password from the request data
-        username = request.data.get('username')
+        empid = request.data.get('empid')
         password = request.data.get('password')
 
         # Perform user authentication (You can use Django's built-in authentication or your own method)
         try:
-            user = User.objects.get(username=username, password=password)
+            user = User.objects.get(empid=empid, password=password)
         except User.DoesNotExist:
             return JsonResponse({'error': 'Invalid username or password'}, status=400)
 
